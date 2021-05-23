bb025e31e3dc32c5b0c20a03381235ef15b7cd31
janeseed <tjdus@gmail.com>
2021-05-24 04:02:38 +0900

    revise checkable tag

--- src/SongPref.js
+++ src/SongPref.js
@@ -1,18 +1,34 @@
 import React from "react";
-import { Button, Typography, Card, Space } from "antd";
+import { Button, Typography, Card, Space, Tag } from "antd";
 import { HeartOutlined, LeftOutlined } from "@ant-design/icons";
 import { withRouter } from "react-router-dom";
 import "./css/start.css";
 import "./css/songpref.css";
 
 const { Paragraph } = Typography;
+const { CheckableTag } = Tag;
+
+const songPrefTags = ["songId1","songId2", "songId3"];
 
 class SongPref extends React.Component {
+  
+  state = {
+    selectedSongs: [],
+  };
+
+  handleSongPref(tag, checked) {
+    const { selectedSongs } = this.state;
+    const nextSelectedTags = checked ? [...selectedSongs, tag] : selectedSongs.filter(t => t !== tag);
+    console.log('You are interested in: ', nextSelectedTags);
+    this.setState({ selectedSongs: nextSelectedTags });
+  }
+
   nextPath(path) {
     this.props.history.push(path);
   }
 
   render() {
+    const { selectedSongs } = this.state;
     return (
       <div>
         <Button
@@ -30,15 +46,24 @@ class SongPref extends React.Component {
         </Paragraph>
         <div className="space-div">
           <Space>
-            <Card size="small" title="문어의 꿈" extra={<Button shape="circle" icon={<HeartOutlined />} />} style={{ width: 160 }}>
-              <p>안예은</p>
-            </Card>
-            <Card size="small" title="문어의 꿈" extra={<Button shape="circle" icon={<HeartOutlined />} />} style={{ width: 160 }}>
-              <p>안예은</p>
-            </Card>
-            <Card size="small" title="문어의 꿈" extra={<Button shape="circle" icon={<HeartOutlined />} />} style={{ width: 160 }}>
-              <p>안예은</p>
-            </Card>
+            {songPrefTags.map((tag) => (
+              <Card
+              size="small"
+              title="문어의 꿈"
+              extra={
+                <CheckableTag
+                  key={tag}
+                  checked={selectedSongs.indexOf(tag) > -1}
+                  onChange={checked => this.handleSongPref(tag, checked)}
+                >
+                  <HeartOutlined />
+                </CheckableTag>
+              }
+              style={{ width: 160 }}
+              >
+                <p>안예은</p>
+              </Card>
+            ))}
           </Space>
         </div>
         <Button
