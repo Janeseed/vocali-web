7c7bb1624a5de252cde53e7edbe2559ce40cd483
min0623 <confuoco1023@gmail.com>
2021-06-01 23:23:03 +0900

    Rollback router

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import BrowserRouter from 'react-router-dom/BrowserRouter'
 
 import App from "./App";
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-     <App />
-   </BrowserRouter>,
+    <App />,
     document.getElementById('root')
 );
